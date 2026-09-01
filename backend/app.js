d4872f3be7794ad01d2609d831e5adb631e37f93
Oluwaseyifunmi Banjo <banjoseyi2@gmail.com>
2026-09-01 03:46:21 +0100

    feat:added the setup and also the user model

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-// import DataBase from "./config/DataBase.js";
+import DataBase from "./config/DataBase.js";
+
+// Routes
+import UserRoutes from "./routes/UserRoutes.js";
 
 dotenv.config();
 
@@ -9,6 +12,24 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`);
-});
+const startServer = async () => {
+    try {
+        await DataBase.connectDB();
+
+        const server = app.listen(PORT, () => {
+            console.log(`App listening at http://localhost:${PORT}`);
+        });
+
+        server.on("error", (error) => {
+            console.error("Server error:", error);
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+
+// user
+app.use("/api/user", UserRoutes);
+
+server();
---
+++ backend/model/User.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+            lowercase: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 8,
+            maxlength: 128,
+            select: false
+        },
+        KycType: {
+            type: String,
+            enum: ["bvn", "nin"],
+            default: null,
+        },
+        KycId: {
+            type: String,
+            required: true,
+            trim: true,
+            default: null,
+            select: false
+        },
+        dateOfBirth: {
+            type: Date,
+            default: null
+        },
+        isKycVerified: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+    this.password = await bcrypt.hash(this.password, 11);
+});
+
+userSchema.methods.comparePaswword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+const User = mongoose.model("User", userSchema);
+export default User;
