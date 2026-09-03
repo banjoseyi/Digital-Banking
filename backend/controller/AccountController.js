b9b8b38e5c578498f7ec4dc8915f29d8c127d4ed
Oluwaseyifunmi Banjo <banjoseyi2@gmail.com>
2026-09-03 23:34:54 +0100

    feat: added a transaction statusCheck endpoint and also moved the  transfer logic away from the AmountController to the transactionController

--- backend/controller/AccountController.js
+++ backend/controller/AccountController.js
@@ -1,7 +1,6 @@
 import Account from "../model/Account.js"
 import AppError from "../utils/AppError.js";
 import nibss from "../service/nibssClient.js";
-import Transaction from "../model/Transaction.js";
 
 const createAccount = async (req, res, next) => {
     try {
@@ -136,68 +135,10 @@ const nameEnquiry = async (req, res, next) => {
 }
 
 
-const transferFunds = async (req, res, next) => {
-    try {
 
-        const { to, amount } = req.body;
-
-
-
-        const senderAccountNumber = await Account.findOne({ user: req.user._id, });
-
-        if (!senderAccountNumber) {
-            throw new AppError("Account number is required", 400);
-        }
-
-        if (senderAccountNumber.accountNumber === to) {
-            throw new AppError("You can't transfer funds to yourselft", 400);
-        }
-
-        const { data: nibssResponse } = await nibss.post("/api/transfer", {
-            from: senderAccountNumber.accountNumber,
-            to,
-            amount: String(amount),
-        })
-
-        if (!nibssResponse.reference) {
-            throw new AppError(nibssResponse.message || "Transfer failed", 400);
-        }
-
-        await Transaction.create({
-            user: req.user._id,
-            transactionId: nibssResponse.reference,
-            from: senderAccountNumber.accountNumber,
-            to: nibssResponse.receiverAccount,
-            amount: nibssResponse.amount,
-            status: nibssResponse.status
-        })
-
-        return res.status(200).json({
-            success: true,
-            message: "Transfer successful",
-            data: {
-                transactionId: nibssResponse.reference,
-                from: senderAccountNumber.accountNumber,
-                to: nibssResponse.receiverAccount,
-                amount: nibssResponse.amount,
-                status: nibssResponse.status,
-            },
-        });
-
-    } catch (error) {
-        if (error.response) {
-            return next(new AppError(
-                error.response.data?.message || "Unable to process transfer",
-                error.response.status || 500
-            ));
-        }
-        next(error);
-    }
-}
 
 export default {
     createAccount,
     getBalance,
-    nameEnquiry,
-    transferFunds
+    nameEnquiry
 }
